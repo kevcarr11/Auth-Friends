e033d51a724a1e6807994d809c819804bd7de065
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-11-03 17:01:06 -0600

    successful post request to server

--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react'
+import axios from "axios"
 
 function Login(props) {
-  const [data, setData] = useState({
-    email: "",
+  const [error, setError] = useState()
+  const [userInfo, setUserInfo] = useState({
+    username: "",
     password: "",
   })
 
   const handleChange = (event) => {
-    setData({
-      ...data,
+    setUserInfo({
+      ...userInfo,
       [event.target.name]: event.target.value,
     })
   } 
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    axios.post("http://localhost:5000/api/login", userInfo)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err =>  {
+        setError(err.response.data.error)
+      })
+  }
+
   return (
     <div>
-      <form>
-        <input type="email" name="email" placeholder="Email" value={data.email} onChange={handleChange} />
-        <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+      {error && <div className="error">{error}</div>}
+
+        <input type="text" name="username" placeholder="User Name" value={userInfo.username} onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" value={userInfo.password} onChange={handleChange} />
 
         <button type="submit">Login</button>
       </form>
